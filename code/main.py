51251b35903ae1d8fafbb655c0f9a7f213a680a6
prithudasgupta <prithu_dasgupta@brown.edu>
2020-04-22 16:53:57 -0700

    Removed extra images

--- code/main.py
+++ code/main.py
@@ -18,12 +18,6 @@ def test(model, test_data):
     )
 
 def main():
-    img = cv2.imread('testImage.jpg',1)
-        # img = cv2.imread('0b86cecf-67d1-4fc0-87c9-b36b0ee228bb.jpg', 1)
-        # img = cv2.imread('12c6cb72-3ea3-49e7-b381-e0cdfc5e8960.jpg', 1)
-        # img = cv2.imread('12c6cb72-3ea3-49e7-b381-e0cdfc5e8960.jpg', 1)
-        # img = cv2.imread('1e241dc8-8f18-4955-8988-03a0ab49f813.jpg', 1)
-        # res = get_bounding_box(img)
 
     # Take note of how txt file in data directory must be formatted for every jpg file included in dataset!! Also, as of rn, must be jpg file but not hard to incorporate other file types.
     train_images, train_labels, test_images, test_labels = preprocess.parse_images_and_labels(DATA_DIR, TRAIN_TEST_RATIO)
--- code/model.py
+++ code/model.py
@@ -1,14 +1,29 @@
 import tensorflow as tf
 import hyperparameters as hp
 from tensorflow.keras.layers import \
-        Conv2D, MaxPool2D, Dropout, Flatten, Dense
+        Conv2D, MaxPool2D, Dropout, Flatten, Dense, Reshape, Softmax
 
-
+voc_size = 36
 
 class Model(tf.keras.Model):
-    """ NN model for license plate detection """
-    def __init__(self):
+    """ 
+    NN model for license plate detection 
+    
+    seq_len: Number of characters in license plate (~6/7)
+    vocab_size: Number of possible characters (~(26 + 10))
+
+
+    """
+    def __init__(self, seq_len, vocab_size):
         super(Model, self).__init__()
+
+        # logits should be batch_size by seq_len by vocab_size
+        # labels will be batch_size by seq_len
+        # flatten both for cross entropy loss
+        # last dense layer should be seq_len * vocab_size
+        self.seq_len = seq_len
+        self.vocab_size = vocab_size
+        voc_size = vocab_size
 
         self.optimizer = tf.keras.optimizers.RMSprop(
             learning_rate=hp.learning_rate,
@@ -21,7 +36,9 @@ class Model(tf.keras.Model):
             Flatten(),
             Dropout(0.3),
             Dense(1024, activation="relu"),
-            Dense(1000, activation="softmax")
+            Dense(self.seq_len * self.vocab_size),
+            Reshape((self.seq_len, self.vocab_size)),
+            Softmax(axis=-1)
         ]
 
     def call(self, img):
@@ -32,6 +49,14 @@ class Model(tf.keras.Model):
 
     @staticmethod
     def loss_fn(labels, predictions):
-        """ Loss function for the model. """
+        """ 
+        Loss function for the model. 
+        
+        Labels are batch_size by seq_len
+        Predictions are batch_size by seq_len by vocab_size
+        """
+        flattened_labels = tf.reshape(labels, [-1])
+        # Probably a better way to write this but this should work for now
+        flattened_predictions = tf.reshape(predictions, [-1, voc_size])
         return tf.keras.losses.sparse_categorical_crossentropy(
-            labels, predictions, from_logits=False)
+            flattened_labels, flattened_predictions, from_logits=False)
