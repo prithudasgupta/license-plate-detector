238adf880a4b4fd3d5e255a18d4105827ebd2da8
prithudasgupta <prithu_dasgupta@brown.edu>
2020-05-01 10:42:15 -0700

    Segments characters left to right

--- code/segmentation.py
+++ code/segmentation.py
@@ -18,11 +18,12 @@ def findCharacterContour(img):
 
     _,threshold = cv2.threshold(gray_plate, 140, 255, 0)
     contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_ctrs = sorted(contours, key=lambda ctr: cv2.boundingRect(ctr)[1])
 
     w,h,x,y = 0,0,0,0
 
     print("%s contours found.", str(len(contours)))
-    for contour in contours:
+    for contour in sorted_ctrs:
         area = cv2.contourArea(contour)
 
         # rough range of areas of a plate number
@@ -32,14 +33,13 @@ def findCharacterContour(img):
         # rough dimensions of a character
         if h > 20 and h < 90 and w > 10 and w < 50:
             character = cropCharacter(img, [x,y,w,h])
-            plate_characters.append([x, character])
+            plate_characters.append(character)
             cv2.rectangle(img, (x,y), (x+w, y+h), (0,0,255), 1)
             cv2.imshow('image',character)
             cv2.waitKey(0)
 
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
     print("%s plate characters found", str(len(plate_characters)))
 		
-
-
-#histogram(cv2.imread("data_license_only/crop_h1/I00000.png", 1))
 findCharacterContour(cv2.imread("data_license_only/crop_h1/I00000.png", 1))
