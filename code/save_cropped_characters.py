7ba8587b43cf7199664526a8acc3bd875196ee53
prithudasgupta <prithu_dasgupta@brown.edu>
2020-05-07 18:28:48 -0700

    Conversion script additions

--- code/save_cropped_characters.py
+++ code/save_cropped_characters.py
@@ -1,3 +1,8 @@
+import glob, io, cv2, numpy as np
+from csv import reader
+from segmentation import findCharacterContour
+import tensorflow as tf
+
 vals = open('data_license_only/trainVal.csv', 'r')
 lines = reader(vals)
 count = 0
@@ -14,4 +19,6 @@ for row in lines:
     character_contours = findCharacterContour(img)
 
     for i in range(min(len(character_contours), len(plate_number))):
-        cv2.imwrite('data_segmented/' + str(count) + '_' + plate_number[i]  + '.jpg', character_contours[i, :, :])
+        file_name = 'data_segmented/' + str(count) + '_' + plate_number[i]  + '.jpg'
+        cv2.imwrite(file_name, character_contours[i, :, :])
+        print("Wrote to file " + str(file_name))
